eb10c793c42d644776c5a58622b0fd60f8376ddd
ginger-kang <kdhoon07@gmail.com>
2020-08-25 18:19:33 +0900

    :sparkles: class on typescript

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "kang",
-  age: 20,
-  gender: "male",
-};
+const kang = new Human("kang", 20, "male");
 
 const sayHi = (person: Human): string => {
   return `Hi ${person.name}, age: ${person.age}, gender: ${person.gender}!`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(kang));
 
 export {};
