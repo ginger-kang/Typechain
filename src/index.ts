eb6e70f6ba77da4b940d1de49dd07c002860911f
ginger-kang <kdhoon07@gmail.com>
2020-08-29 01:20:37 +0900

    :sparkles: Complete Typechain

--- src/index.ts
+++ src/index.ts
@@ -15,13 +15,13 @@ class Block {
   ): string =>
     CryptoJs.SHA256(index + previousHash + timestamp + data).toString();
 
-  static validateStructure = (aBlock: Block): boolean => {
+  static validateStructure = (aBlock: Block): boolean =>
     typeof aBlock.index === "number" &&
-      typeof aBlock.hash === "string" &&
-      typeof aBlock.previousHash === "string" &&
-      aBlock.timestamp === "number" &&
-      aBlock.data === "string";
-  };
+    typeof aBlock.hash === "string" &&
+    typeof aBlock.previousHash === "string" &&
+    typeof aBlock.timestamp === "number" &&
+    typeof aBlock.data === "string";
+
   constructor(
     index: number,
     hash: string,
@@ -65,12 +65,42 @@ const createNewBlock = (data: string): Block => {
     newTimestamp
   );
 
+  addBlock(newBlock);
   return newBlock;
 };
 
-const isBlockValid = (
-  candidateBlock: Block,
-  previousBlock: Block
-): boolean => {};
+const getHashforBlock = (aBlock: Block) =>
+  Block.calculateBlockHash(
+    aBlock.index,
+    aBlock.previousHash,
+    aBlock.timestamp,
+    aBlock.data
+  );
+
+const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
+  if (!Block.validateStructure(candidateBlock)) {
+    return false;
+  } else if (previousBlock.index + 1 !== candidateBlock.index) {
+    return false;
+  } else if (previousBlock.hash !== candidateBlock.previousHash) {
+    return false;
+  } else if (getHashforBlock(candidateBlock) !== candidateBlock.hash) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const addBlock = (candidateBlock: Block): void => {
+  if (isBlockValid(candidateBlock, getLatestBlock())) {
+    blockchain.push(candidateBlock);
+  }
+};
+
+createNewBlock("second");
+createNewBlock("third");
+createNewBlock("fourth");
+
+console.log(blockchain);
 
 export {};
