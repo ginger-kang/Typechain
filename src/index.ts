910405e2e61f8727057ae14ccafa8edeac0df647
ginger-kang <kdhoon07@gmail.com>
2020-08-28 23:47:42 +0900

    :bricks: Creating a Block part two

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
+import * as CryptoJs from "crypto-js";
+
 class Block {
   public hash: string;
   public index: number;
   public previousHash: string;
   public data: string;
   public timestamp: number;
+
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string =>
+    CryptoJs.SHA256(index + previousHash + timestamp + data).toString();
 
   constructor(
     index: number,
