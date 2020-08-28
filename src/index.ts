a53f3a6330a21b6708398b3139800b2c49daebf1
ginger-kang <kdhoon07@gmail.com>
2020-08-28 23:39:33 +0900

    :bricks: Creating a Block

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,29 @@
-class Human {
-  public name: string;
-  public age: number;
-  public gender: string;
-  constructor(name: string, age: number, gender: string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+class Block {
+  public hash: string;
+  public index: number;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
   }
 }
 
-const kang = new Human("kang", 20, "male");
+const genesisBlock: Block = new Block(0, "2020", "", "Hi", 123);
 
-const sayHi = (person: Human): string => {
-  return `Hi ${person.name}, age: ${person.age}, gender: ${person.gender}!`;
-};
+let blockchain: [Block] = [genesisBlock];
 
-console.log(sayHi(kang));
+console.log(blockchain);
 
 export {};
